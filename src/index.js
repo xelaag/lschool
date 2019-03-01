4fb4290d9db42abd8c3ad6f3545e625b474b91e7
Alexander Bakelev <abakelev@mtt.ru>
2019-03-01 13:24:04 +0300

    chenge: 6

--- src/index.js
+++ src/index.js
@@ -73,7 +73,11 @@ function delegate(target, fn) {
    once(document.querySelector('button'), () => console.log('обработчик выполнился!')) // добавит такой обработчик кликов для указанного элемента, который вызовется только один раз и затем удалится
  */
 function once(target, fn) {
-    target.addEventListener('click', fn, { once: true });
+    function handler(event) {
+        fn(event);
+        target.removeEventListener('click', handler);
+    }
+    target.addEventListener('click', handler);
 }
 
 export {
