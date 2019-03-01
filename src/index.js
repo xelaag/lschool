d638c412ee11bf4c3c04d4b253be6fb8b6439dfa
Alexander Bakelev <abakelev@mtt.ru>
2019-03-01 14:22:56 +0300

    fix: 4

--- src/index.js
+++ src/index.js
@@ -47,7 +47,9 @@ function skipDefault(eventName, target) {
    emulateClick(document.querySelector('a')) // для указанного элемента должно быть сэмулировано события click
  */
 function emulateClick(target) {
-    target.click();
+    var event = new Event('click');
+
+    target.dispatchEvent(event);
 }
 
 /*
